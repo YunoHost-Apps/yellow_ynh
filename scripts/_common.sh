a784576bafc3a821594908c75a9f3136a251e3b5
ericgaspar <junk.eg@free.fr>
2020-11-01 21:22:57 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,9 +6,6 @@
 
 YNH_PHP_VERSION="7.3"
 
-# dependencies used by the app
-pkg_dependencies=""
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
