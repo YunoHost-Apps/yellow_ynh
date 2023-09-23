5ad6854f311c28466bf55f21cfa125ab4c3737e1
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-09-23 14:24:21 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-#REMOVEME? YNH_PHP_VERSION="8.0"
-
-#REMOVEME? pkg_dependencies="php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xmlrpc php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-soap php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-zip"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
